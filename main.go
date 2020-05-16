eef8e7d81f16eba9611c78149c77cbf87c12a5ed
风之凌殇 <fzls.zju@gmail.com>
2020-05-17 01:40:09 +0800

    增加中文支持

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"os"
+
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
 func main() {
+	err := os.Setenv("FYNE_FONT", "fonts/YaHei.Consolas.1.11b.ttf")
+	if err != nil {
+		return
+	}
+	defer os.Unsetenv("FYNE_FONT")
+
 	myApp := app.New()
 
-	w := myApp.NewWindow("Hello")
-	w.SetContent(widget.NewLabel("Hello Fyne!"))
+	w := myApp.NewWindow("你好 Hello World!")
+	w.SetContent(widget.NewLabel("你好 Hello World!"))
+	w.Resize(fyne.Size{500,400})
 
 	w.ShowAndRun()
 }
